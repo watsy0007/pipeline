410ca0bb6fc45ed2d264fa7f745400530970c6a4
watsy0007 <watsy0007@watsy0007mbp.local>
2018-02-02 14:41:05 +0800

    request connection close

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'project',
-    version      = '1.6',
+    version      = '1.7',
     packages     = find_packages(),
     entry_points = {'scrapy': ['settings = pipeline.settings']},
 )
