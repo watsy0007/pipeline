af8d82342fdeee84c37901aba6a745cd7e47a617
watsy0007 <watsy0007@watsy0007mbp.local>
2018-02-04 22:45:08 +0800

    google translate

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'project',
-    version      = '1.10',
+    version      = '1.20',
     packages     = find_packages(),
+    include_package_data = True,
     entry_points = {'scrapy': ['settings = pipeline.settings']},
 )
