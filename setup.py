04d353b8ff0720739a51cd8d80faa4601b1d92eb
watsy0007 <watsy0007@watsy0007mbp.local>
2018-03-03 00:15:27 +0800

    print price

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'project',
-    version      = '1.29',
+    version      = '1.30',
     packages     = find_packages(),
     include_package_data = True,
     entry_points = {'scrapy': ['settings = pipeline.settings']},
